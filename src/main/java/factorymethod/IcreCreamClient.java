2166fdcc0f8ba8da60362a0cc77b328d51067036
victor-alex159 <alexbenavente322@gmail.com>
2021-03-12 21:32:04 -0500

    agrego clases

--- src/main/java/factorymethod/IcreCreamClient.java
+++ src/main/java/factorymethod/IcreCreamClient.java
@@ -1,10 +1,36 @@
 package factorymethod;
 
+import java.util.Scanner;
+
 import factorymethod.factory.IceCreamFactory;
+import factorymethod.icecream.IceCreamMachine;
 
 public class IcreCreamClient {
+    
+    public static void main(String[] args) {
+		System.out.println("Enter your choice of ice cream");
+		System.out.println("1-chocolate, 2- vainilla, 3- fresa, 4- exit");
+		Scanner scanner = new Scanner(System.in);
+		String type = scanner.nextLine();
+		System.out.println("\n Enter your choice of toppings");
+		String topping = scanner.nextLine();
 
-    private IceCreamFactory factory;
+		IceCreamMachine iceCream = null;
+		while(!type.equalsIgnoreCase("4")) {
+			iceCream = IceCreamFactory.getInstance(type);
+			iceCream.setTopping(topping);
+			iceCream.prepareIceCream();
+			
+			System.out.println("\n Enter your choice of ice cream again");
+			type = scanner.nextLine();
+			if(!type.equalsIgnoreCase("4") ) {
+				System.out.println("\n Enter your choice of toppings again");
+				topping = scanner.nextLine();
+			}
+		}
+		System.out.println("\nThank You ");
+
+	}
 
 
 }
--- src/main/java/factorymethod/factory/IceCreamFactory.java
+++ src/main/java/factorymethod/factory/IceCreamFactory.java
@@ -1,6 +1,22 @@
 package factorymethod.factory;
 
+import factorymethod.icecream.IceCreamMachine;
+import factorymethod.icecream.impl.IceCreamFresa;
+import factorymethod.icecream.impl.IceCreamVainilla;
+import factorymethod.icecream.impl.IcreCreamChocolate;
+
 public class IceCreamFactory {
+	
+	public static IceCreamMachine getInstance(String type) {
+		
+		switch (type) {
+			case "1": return new IcreCreamChocolate();
+			case "2": return new IceCreamVainilla();
+			case "3": return new IceCreamFresa();
+		}
+		return null;
+		
+	}
 
 
 }
